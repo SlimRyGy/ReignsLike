33b82ebbbb1d696ab94d9bae25a40da26001febd
JeromeJ <jerome.jamart.42@gmail.com>
2018-01-04 10:31:47 +0100

    Save model

---
+++ Assets/Scripts/ScriptableObjects/Save.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Save : ScriptableObject
+{
+    #region Public Members
+
+    public List<float> fillAmounts = new List<float>();
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    #endregion
+
+    #region Class Methods
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
